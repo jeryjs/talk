ac7b1343cc761a617959c4911a91aa3d1d1c4d81
Jery <jery99961@gmail.com>
2023-04-07 08:01:52 +0530

    Change to GoLang

---
+++ app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	su "talk/speechUtils"
+)
+
+func scan() string {
+    scanner := bufio.NewScanner(os.Stdin)
+    if scanner.Scan() {return scanner.Text()}
+    if err := scanner.Err(); err != nil {fmt.Fprintln(os.Stderr, err)}
+    return ""
+}
+
+func main() {
+	var msg string
+	for(msg != "exit") {
+		msg = su.Listen()
+		text := su.Chat(msg)
+		su.Say(text)
+	}
+	log.Println("\n\n"+msg)
+}
