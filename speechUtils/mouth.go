ad0fbca4beb175edefeb972076f6ecb85dfd146c
Jery <jery99961@gmail.com>
2023-04-07 09:49:04 +0530

    Espeak f3 voice complete

--- speechUtils/mouth.go
+++ speechUtils/mouth.go
@@ -1,18 +1,32 @@
 package speechUtils
 
 import (
+	_ "embed"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
-// Implement Text-To-Speech using eSpeak.exe.
-// espeak.exe must exist in same directory as main.go
+//go:embed ..\assets\espeak.exe
+var espeakBinary []byte
+
 func Say(text string) {
 	fmt.Println(text+"\n")
-    var msg = text
-    cmd := exec.Command("./espeak.exe", msg)
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+
+	binaryPath := filepath.Join(os.TempDir(), "espeak.exe")
+	if err := ioutil.WriteFile(binaryPath, espeakBinary, 0755); err != nil {
+		fmt.Println("Error writing binary file:", err)
+		return
+	}
+
+	cmd := exec.Command(binaryPath, "-v", "en+f3", "-p", "70", "-s", "200", text)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error running command:", err)
+		return
+	}
+
+	fmt.Println(string(output))
 }
